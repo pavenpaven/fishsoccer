84486fdbeed280a4387d538f5c6a0082793cabb9
Mattias Burman <mmburman@hotmail.com>
2025-12-20 11:14:32 +0100

    turn turn turn turn

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,10 +4,18 @@ extends CharacterBody2D
 @export var bouncecol = 2.0
 @export var drag      = 0.4
 @export var kickpower = 1.0
+var animation = null
+
+func _ready():
+	animation = get_node("animation")
 
 func _physics_process(delta):
-	velocity += (Globals.ball_pos - position).normalized() * accel *delta
+	velocity += (Globals.ball_pos - position).normalized() * accel * delta
 	velocity = (1 - delta*drag)*velocity
+	if (Globals.ball_pos - position).x < 0:
+		animation.flip_h = true
+	else:
+		animation.flip_h = false
 
 	var collision_info = move_and_collide(velocity*delta)
 
