405d62616552397f6a577b09d8ab67ba2f774c6d
Mattias Burman <mmburman@hotmail.com>
2025-12-20 10:26:39 +0100

    animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
-@export var accel = 2
-@export var bouncecol = 2
+@export var accel     = 200
+@export var bouncecol = 2.0
+@export var drag      = 0.4
+@export var kickpower = 1.0
 
 func _physics_process(delta):
-	velocity += (Globals.ball_pos - position).normalized() * accel
+	velocity += (Globals.ball_pos - position).normalized() * accel *delta
+	velocity = (1 - delta*drag)*velocity
 
 	var collision_info = move_and_collide(velocity*delta)
 
 	if collision_info:
 		if collision_info.get_collider().name=="Ball":
-			collision_info.get_collider().velocity += velocity.normalized()*(velocity.normalized().dot(velocity) - velocity.normalized().dot(collision_info.get_collider().velocity)) 
+			var b = collision_info.get_collider().velocity
+			collision_info.get_collider().velocity += velocity.normalized()*(velocity.normalized().dot(velocity - b)) * kickpower
 		velocity = velocity.bounce(collision_info.get_normal()) / bouncecol
