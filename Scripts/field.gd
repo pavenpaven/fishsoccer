88f37a6dfc2ec62cd47b2a1ea5b1bb5545a2e4c7
Mattias Burman <mmburman@hotmail.com>
2025-12-21 20:35:20 +0100

    balls and goals

--- Scripts/field.gd
+++ Scripts/field.gd
@@ -7,5 +7,28 @@ func _input(event):
 			var instance = scene.instantiate()		
 			add_child(instance)
 			instance.position = (event.position + Vector2(65,10)) / 2
-			print(instance.position)
-			print("added instance")
+	if event is InputEventKey:
+		if event.keycode == KEY_F and event.pressed:
+			var pos  = get_viewport().get_mouse_position()
+			var scene = load("res://Scenes/ball.tscn")
+			var instance = scene.instantiate()
+			add_child(instance)
+			instance.position = (pos + Vector2(65,10)) / 2
+			instance.is_fake = true
+			instance.get_node("AnimatedSprite2D").animation = "decoy"
+					
+func reload():
+	var balls = Globals.balls
+	Globals.balls = []
+	for i in balls:
+		if not i.is_fake:
+			Globals.balls.append(i)
+			i.position = Vector2(280, 160)
+			i.velocity = Vector2(0,0)
+
+func _on_purple_goal_goal() -> void:
+	reload()
+
+
+func _on_orange_goal_goal() -> void:
+	reload()
