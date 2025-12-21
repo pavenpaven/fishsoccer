0a2655fd26f50066b829853bbce41a4341318d74
Mattias Burman <mmburman@hotmail.com>
2025-12-21 01:08:11 +0100

    added basic kick

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,11 +2,12 @@ class_name FotballPlayer extends CharacterBody2D
 
 @export var accel     = 500
 @export var bouncecol = 1.5
-@export var drag      = 1
-@export var falldowndrag = 0.4
-@export var kickpower = 1.0
+@export var drag      = 0.7
+@export var falldowndrag = 1
+@export var kickpower = 100.0
+@export var pushpower = 0.5
 @export var seagrassstun = 2.0
-@export var unprecissionshot = 30.0
+@export var unprecissionshot = 100.0
 @export var isorange         = true
 @export var aitan            = 1
 @export var behindball       = 20
@@ -19,6 +20,12 @@ var is_down = false
 var timer = null
 var collision = true
 
+
+func sigmoid(x):
+	var a = ((exp(x)/(exp(x) + 1)) - 0.5)*2
+	print(x, "  :  ",a)
+	return a
+
 func seagrassed():
 	if isorange:
 		animation.animation = "ofall"
@@ -30,14 +37,15 @@ func seagrassed():
 
 func _ready():
 	animation = get_node("animation")
+	isorange = not isorange
 	if not isorange:
 		animation.animation = "pdefault"
-	timer     = $Timer	
+	timer = $Timer	
 
 func rot(v):
 	return Vector2(-v.y, v.x)
 	
-func ai():	
+func ai_move_dir():	
 	var center = position #+ Vector2(16, 16)
 	var target
 	if isorange:
@@ -60,15 +68,27 @@ func ai():
 			animation.animation="odefault"
 		return Globals.ball_pos - center
 	elif prop > aitan:  # this cas is when player is infront of ball
+		if aidebug:
+			animation.animation="pdefault"
 		return (Globals.ball_pos + rot(dir) * sign(xalg) * sideball - center)
 	else: # this case is when player is beside the ball
 		if aidebug:
 			animation.animation="placeholder"
 		return (Globals.ball_pos - behindball*dir.normalized() - center)
-		
+
+func ai_kick():
+	var target
+	if isorange:
+		target = Globals.ogoal
+	else:
+		target = Globals.pgoal
+	var b = Globals.ball_pos
+	return (target - position).normalized()*kickpower*(sigmoid(abs(velocity.normalized().dot(velocity - b))/100)*3 + 1)
+	#return velocity.normalized()*(velocity.normalized().dot(velocity - b)) * kickpower
+	
 func _physics_process(delta):
 	if not is_down:
-		velocity += ai().normalized() * accel * delta * Globals.physics_speed
+		velocity += ai_move_dir().normalized() * accel * delta * Globals.physics_speed
 	else:
 		velocity = (1 - delta*falldowndrag*Globals.physics_speed)*velocity
 	velocity = (1 - delta*drag)*velocity
@@ -82,7 +102,10 @@ func _physics_process(delta):
 	if collision_info:
 		if collision_info.get_collider().is_class("CharacterBody2D") and collision:
 			var b = collision_info.get_collider().velocity
-			collision_info.get_collider().velocity += velocity.normalized()*(velocity.normalized().dot(velocity - b)) * kickpower + Vector2(randfn(-unprecissionshot, unprecissionshot), randfn(-unprecissionshot, unprecissionshot))
+			if collision_info.get_collider().is_ball:
+				collision_info.get_collider().velocity += ai_kick()
+			elif collision_info.get_collider().is_player:
+				collision_info.get_collider().velocity += velocity.normalized()*(velocity.normalized().dot(velocity - b)) * pushpower
 		velocity = velocity.bounce(collision_info.get_normal()) / bouncecol
 
 
